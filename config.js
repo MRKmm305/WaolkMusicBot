23ad171e9a8c99c31bbbf6b9ea111153fce94c67
MRKmm305 <mykingop9098@gmail.com>
2025-06-16 09:29:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,14 +5,14 @@ module.exports = {
   language: "en",
   ownerID: ["1004206704994566164", ""], 
   mongodbUri : "",
-  spotifyClientId : "",
+  spotifyClientId : "mongodb+srv://mrk100:krishna10@waolkbot.tugxhwk.mongodb.net/?retryWrites=true&w=majority",
   spotifyClientSecret : "",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  activityName: "YouTube Music", 
+  activityName: "Music", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  SupportServer: "https://discord.gg/UQCE4fddaN",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
